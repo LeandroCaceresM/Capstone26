8f5882f7dd9a187f0788e8647dded8528f229e1b
LeandroCaceresM <le.caceres@duocuc.cl>
2026-06-24 19:28:46 -0400

    ajustes en settings para ocultar datos sensibles

--- FASE 2/Evidencia Proyecto/Pagina_Capstone/Proyecto/core/UrbanLink/settings.py
+++ FASE 2/Evidencia Proyecto/Pagina_Capstone/Proyecto/core/UrbanLink/settings.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
 
 from pathlib import Path
-import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,17 +80,17 @@ WSGI_APPLICATION = 'UrbanLink.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'kjkszpjhesoyamyecgaa',
-        'HOST': 'db.linljyuykgncuvnrfagx.supabase.co',
-        'PORT': '5432',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
 
-SUPABASE_URL = "https://linljyuykgncuvnrfagx.supabase.co"
-SUPABASE_ANON_KEY = "sb_publishable_IVBUZxPj2swgMEDRN5_v4g_--YMqkHM"
-
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_ANON_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
